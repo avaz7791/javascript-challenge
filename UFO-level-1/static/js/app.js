bba6eeae17f5e9df561f4cf737f59b64bee8ca59
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-03-07 23:53:07 -0800

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,8 +3,12 @@ var tableData = data;
 
 // use tbody which is empty now
 // add an id to this section in the index.
-var tbody_section = d3.select("tbody");
+var tbody_section = d3.select("#table1");
+var filter_btn = d3.select("#filter-btn")
+var form1 = d3.select("form")
 
+
+// Use data and go through each row
 tableData.forEach(function(ufo_sights){
     var row = tbody_section.append("tr")
 
@@ -17,3 +21,35 @@ tableData.forEach(function(ufo_sights){
 
 
 
+form1.on("submit", filter_tbl);
+filter_btn.on("click", filter_tbl);
+
+
+function filter_tbl() {
+
+    //do not let the page to refresh automatically with this command
+    d3.event.preventDefault();
+
+    var dateToFilter = d3.select("#datetime");
+
+    var dt_value = dateToFilter.property("value");
+
+    //var filteredData = people.filter(person => person.bloodType === inputValue);
+    var filteredData = tableData.filter( x => x.datetime === dt_value);
+    
+    tableData.forEach(function() {
+        d3.select("#table1").remove();
+
+    });
+
+    filteredData.forEach(function(tbl){
+        var row = tbody_section.append("tr")
+        Object.entries(tbl).forEach(function([key, value]){
+
+            var col = row.append("td");
+            col.text(value);
+        } );
+    });
+
+  
+};
